20977eaf172c04ac7086d2cdd59449b6c722b1a3
Ahmed EBEN HASSINE <ahmedbhs123@gmail.com>
2024-12-16 18:44:06 +0100

    feat: add object locking to prevent concurrent edit conflicts

--- src/Config/Option/EA.php
+++ src/Config/Option/EA.php
@@ -32,4 +32,5 @@ final class EA
     /** @deprecated this parameter is no longer used because menu items are now highlighted automatically */
     public const SUBMENU_INDEX = 'submenuIndex';
     public const URL_SIGNATURE = 'signature';
+    public const LOCK_VERSION = '_lock_version';
 }
