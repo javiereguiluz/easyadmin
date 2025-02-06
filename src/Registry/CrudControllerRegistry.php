d54b6a7c8cd73140497f4970019ceb1b3f064a4f
Javier Eguiluz <javier.eguiluz@gmail.com>
2025-02-06 20:42:47 +0100

    Mark all controller registries as deprecated

--- src/Registry/CrudControllerRegistry.php
+++ src/Registry/CrudControllerRegistry.php
@@ -4,6 +4,8 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Registry;
 
 /**
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ *
+ * @deprecated since 4.25.0, will be removed in EasyAdmin 5.0.0. This registry is generally not needed by developers in newer versions of EasyAdmin. If you require similar functionality, use the equivalent item in the Symfony Cache pool managed by EasyAdmin (inject the "cache.easyadmin" service).
  */
 final class CrudControllerRegistry
 {
--- src/Registry/DashboardControllerRegistry.php
+++ src/Registry/DashboardControllerRegistry.php
@@ -5,6 +5,9 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Registry;
 use EasyCorp\Bundle\EasyAdminBundle\Cache\CacheWarmer;
 use function Symfony\Component\String\u;
 
+/**
+ * @deprecated since 4.25.0, will be removed in EasyAdmin 5.0.0. This registry is generally not needed by developers in newer versions of EasyAdmin. If you require similar functionality, use the equivalent item in the Symfony Cache pool managed by EasyAdmin (inject the "cache.easyadmin" service).
+ */
 final class DashboardControllerRegistry implements DashboardControllerRegistryInterface
 {
     private array $controllerFqcnToRouteMap = [];
