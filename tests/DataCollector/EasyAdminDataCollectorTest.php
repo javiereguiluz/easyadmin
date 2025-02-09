f4613c287ed2f66e129b0fb2d5a88b6caecf5a23
Javier Eguiluz <javier.eguiluz@gmail.com>
2025-02-09 11:35:00 +0100

    Update tests related to DataCollector

--- tests/DataCollector/EasyAdminDataCollectorTest.php
+++ tests/DataCollector/EasyAdminDataCollectorTest.php
@@ -2,84 +2,33 @@
 
 namespace EasyCorp\Bundle\EasyAdminBundle\Tests\DataCollector;
 
+use EasyCorp\Bundle\EasyAdminBundle\Inspector\DataCollector;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class EasyAdminDataCollectorTest extends WebTestCase
 {
-    protected static $options = ['environment' => 'default_backend'];
-
-    public function testCollectorIsEnabled()
+    public function testCollectorIsEnabled(): void
     {
-        $this->markTestSkipped('This test should be updated or deleted...');
+        $client = static::createClient();
+        $client->followRedirects();
+        $client->enableProfiler();
 
-        static::$client->enableProfiler();
-        $this->requestListView();
-
-        $this->assertNotNull(static::$client->getProfile()->getCollector('easyadmin'));
+        $client->request('GET', '/admin');
+        $this->assertNotNull($client->getProfile()->getCollector('easyadmin'));
     }
 
-    public function testCollectorInformationForListView()
+    public function testCollectorData(): void
     {
-        $this->markTestSkipped('This test should be updated or deleted...');
+        $client = static::createClient();
+        $client->followRedirects();
+        $client->enableProfiler();
 
-        static::$client->enableProfiler();
-        $this->requestListView();
-        $collector = static::$client->getProfile()->getCollector('easyadmin');
+        $client->request('GET', '/admin/category');
+        /** @var DataCollector $collector */
+        $collector = $client->getProfile()->getCollector('easyadmin');
+        $collectedData = $collector->getData();
 
-        $this->assertSame(5, $collector->getNumEntities());
-
-        $parameters = [
-            'action' => 'list',
-            'entity' => 'Category',
-            'id' => null,
-            'sort_field' => 'id',
-            'sort_direction' => 'DESC',
-        ];
-        $this->assertSame($parameters, $collector->getRequestParameters());
-
-        $currentConfiguration = $collector->getCurrentEntityConfig();
-        $this->assertSame('Category', $currentConfiguration['name']);
-
-        $backendConfig = $collector->getBackendConfig();
-        $this->assertCount(5, $backendConfig['entities']);
-    }
-
-    public function testCollectorInformationForEditView()
-    {
-        $this->markTestSkipped('This test should be updated or deleted...');
-
-        static::$client->enableProfiler();
-        $this->requestEditView();
-        $collector = static::$client->getProfile()->getCollector('easyadmin');
-
-        $this->assertSame(5, $collector->getNumEntities());
-
-        $parameters = [
-            'action' => 'edit',
-            'entity' => 'Category',
-            'id' => '200',
-            'sort_field' => 'id',
-            'sort_direction' => 'DESC',
-        ];
-        $this->assertSame($parameters, $collector->getRequestParameters());
-
-        $currentConfiguration = $collector->getCurrentEntityConfig();
-        $this->assertSame('Category', $currentConfiguration['name']);
-
-        $backendConfig = $collector->getBackendConfig();
-        $this->assertCount(5, $backendConfig['entities']);
-    }
-
-    public function testCollectorReset()
-    {
-        $this->markTestSkipped('This test should be updated or deleted...');
-
-        static::$client->enableProfiler();
-        $this->requestListView();
-        $collector = static::$client->getProfile()->getCollector('easyadmin');
-
-        $this->assertSame(5, $collector->getNumEntities());
-        $collector->reset();
-        $this->assertSame(0, $collector->getNumEntities());
+        $this->assertSame(['CRUD Controller FQCN', 'CRUD Action', 'Entity ID', 'Sort'], array_keys($collectedData));
+        $this->assertTrue($collector->isEasyAdminRequest());
     }
 }
