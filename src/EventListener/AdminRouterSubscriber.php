3f0f0609693f0486b09b9aa091773d63159e9d84
Matthieu Renard <m.renard@adacis.net>
2025-02-19 19:29:02 +0100

    Fixing issue #6817 (failed to match named routes).

--- src/EventListener/AdminRouterSubscriber.php
+++ src/EventListener/AdminRouterSubscriber.php
@@ -294,6 +294,26 @@ class AdminRouterSubscriber implements EventSubscriberInterface
         // sub-url or sub-path - using the HTTP header x-forwarded-prefix)
         $url = $this->urlGenerator->generate($routeName, $routeParams, UrlGeneratorInterface::RELATIVE_PATH);
 
+        // Now, forge the "absolute" path so that it can be matched to a route.
+        // To do this, we concatenate the relative path to the current request path, and do a basic normalization (only
+        // considering ../).
+        // See issue #6817.
+        $prefix = $request->getPathInfo(); // Our current absolute path, without any subpath prefix
+
+        // Get rid of the page name (/en/route/page --> /en/route)
+        $prefix = substr($prefix, 0, strrpos($prefix, '/'));
+
+        while (str_starts_with($url, '../')) {
+            // Remove the last part of the prefix and the '../' from the path
+            // prefix: /en/route, url: ../anotherRoute --> prefix: /en, url: anotherRoute
+            $prefix = substr($prefix, 0, strrpos($prefix, '/'));
+            $url = substr($url, 3);
+        }
+        // Now, concatenate prefix and path
+        // prefix: /en, url: anotherRoute --> url: /en/anotherRoute
+        // This should now match a route
+        $url = $prefix . '/' . $url;
+
         $newRequest = $request->duplicate();
         $newRequest->attributes->remove('_controller');
         $newRequest->attributes->set('_route', $routeName);
